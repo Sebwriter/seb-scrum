d3e51002aff6db1134053681607af371520841c9
Montibert Sébastien <seb.montibert@gmail.com>
2021-11-10 12:28:09 +0100

    completed Gallery & Contact pages

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,7 +5,7 @@ const  Contact = () => {
           <div className="row"><h1>Contact</h1></div>
           <div className="row">
             <div className="col-12 col-md-6">
-              <img className="img-fluid" src="https://images.pexels.com/photos/3617500/pexels-photo-3617500.jpeg?auto=compress&cs=tinysrgb&h=500&w=840" alt="image aurore boréale"></img>
+              <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d5566.048896378834!2d4.803355794165021!3d45.77070028387888!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xf4170493513eaa9f!2sCCI%20Formation!5e0!3m2!1sfr!2sfr!4v1636543087931!5m2!1sfr!2sfr" width="100%" height="450" allowfullscreen="" loading="lazy"></iframe>
             </div>
             <div className="col-12 col-md-6">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec mattis commodo ante auctor sodales. Praesent maximus condimentum molestie. Duis sit amet eros id justo sodales suscipit. Pellentesque interdum tempus viverra. Sed ut nunc felis. Cras dapibus orci sem, in porta purus rutrum et. Donec neque turpis, vehicula fermentum velit quis, facilisis varius est. Aliquam semper eleifend viverra. Nulla et odio lorem. Pellentesque eget nibh mi. Praesent ornare odio velit, sed luctus ligula auctor eget. Ut ut purus sed neque tincidunt tempor et eget nibh. Suspendisse cursus rhoncus finibus. Quisque quam diam, finibus ut leo ut, rutrum tempor nisl.</div>
           </div>
--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,16 +1,18 @@
-const  Gallery = () => {
-    return(
-        <section>
-        <div className="container-fluid">
-          <div className="row"><h1>Gallery</h1></div>
-          <div className="row">
-            <div className="col-12 col-md-6">
-              <img className="img-fluid" src="https://images.pexels.com/photos/3617500/pexels-photo-3617500.jpeg?auto=compress&cs=tinysrgb&h=500&w=840" alt="image aurore boréale"></img>
-            </div>
-            <div className="col-12 col-md-6">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec mattis commodo ante auctor sodales. Praesent maximus condimentum molestie. Duis sit amet eros id justo sodales suscipit. Pellentesque interdum tempus viverra. Sed ut nunc felis. Cras dapibus orci sem, in porta purus rutrum et. Donec neque turpis, vehicula fermentum velit quis, facilisis varius est. Aliquam semper eleifend viverra. Nulla et odio lorem. Pellentesque eget nibh mi. Praesent ornare odio velit, sed luctus ligula auctor eget. Ut ut purus sed neque tincidunt tempor et eget nibh. Suspendisse cursus rhoncus finibus. Quisque quam diam, finibus ut leo ut, rutrum tempor nisl.</div>
-          </div>
-        </div>
-      </section>
+const Gallery = () => {
+  return (
+    <section>
+      <div className="container-fluid">
+        <div className="row"><h1>Gallery</h1></div>
+        <div className="row">
+          <div className="col-12 col-md-6">
+            <img className="img-fluid" src="https://images.pexels.com/photos/3617500/pexels-photo-3617500.jpeg?auto=compress&cs=tinysrgb&h=500&w=840" alt="image aurore boréale"></img>
+            <img className="img-fluid" src="https://images.pexels.com/photos/346529/pexels-photo-346529.jpeg?auto=compress&cs=tinysrgb&h=500&w=840" alt="image lac montagne"></img>
+            <img className="img-fluid" src="https://images.pexels.com/photos/1257860/pexels-photo-1257860.jpeg?auto=compress&cs=tinysrgb&h=500&w=840" alt="image voie lactée"></img>
+            <img className="img-fluid" src="https://images.pexels.com/photos/1624438/pexels-photo-1624438.jpeg?auto=compress&cs=tinysrgb&h=500&w=840" alt="image lunaire"></img>
+      </div>
+    </div>
+        </div >
+      </section >
     )
 }
 
