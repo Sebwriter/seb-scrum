1367df081f339cc3f564a7d1c704afb21d71f973
Montibert Sébastien <seb.montibert@gmail.com>
2021-11-10 11:34:08 +0100

    Duplicated Home page as Gallery and Contact

--- src/App.js
+++ src/App.js
@@ -4,12 +4,18 @@ import 'bootstrap/dist/js/bootstrap.bundle';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import Home from './components/Home';
+import Gallery from './components/Gallery';
+import Contact from './components/Contact';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      
       <Home />
+      <Gallery />
+      <Contact />
+
       <Footer />
     </div>
   );
---
+++ src/components/Gallery.js
@@ -0,0 +1,17 @@
+const  Gallery = () => {
+    return(
+        <section>
+        <div className="container-fluid">
+          <div className="row"><h1>Gallery</h1></div>
+          <div className="row">
+            <div className="col-12 col-md-6">
+              <img className="img-fluid" src="https://images.pexels.com/photos/3617500/pexels-photo-3617500.jpeg?auto=compress&cs=tinysrgb&h=500&w=840" alt="image aurore boréale"></img>
+            </div>
+            <div className="col-12 col-md-6">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec mattis commodo ante auctor sodales. Praesent maximus condimentum molestie. Duis sit amet eros id justo sodales suscipit. Pellentesque interdum tempus viverra. Sed ut nunc felis. Cras dapibus orci sem, in porta purus rutrum et. Donec neque turpis, vehicula fermentum velit quis, facilisis varius est. Aliquam semper eleifend viverra. Nulla et odio lorem. Pellentesque eget nibh mi. Praesent ornare odio velit, sed luctus ligula auctor eget. Ut ut purus sed neque tincidunt tempor et eget nibh. Suspendisse cursus rhoncus finibus. Quisque quam diam, finibus ut leo ut, rutrum tempor nisl.</div>
+          </div>
+        </div>
+      </section>
+    )
+}
+
+export default Gallery
