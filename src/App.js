c1250a61dce2d107bc26b84e8bbbc01f0efbb463
Montibert Sébastien <seb.montibert@gmail.com>
2021-11-10 11:04:50 +0100

    Added image and text + Created Home page component

--- src/App.js
+++ src/App.js
@@ -3,14 +3,13 @@ import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.bundle';
 import Footer from './components/Footer';
 import Header from './components/Header';
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <section>
-        <h1>Contenu</h1>
-      </section>
+      <Home />
       <Footer />
     </div>
   );
